c0eaf930dc269641cd31c2330a3ce1d9f39216aa
Thibault DESAULES <tdesaules@outlook.com>
2023-12-24 14:44:14 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@ set -o allexport; source .env set; set +o allexport
 
 export NONINTERACTIVE=1
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/tdesaules/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install packages
 
